e49b4a35172c81a7813ffdeb954bc4a46bde8b53
Nikita <tuponixi@gmail.com>
2026-03-15 12:03:16 +0200

    button can throw paper

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("fhsdfosdhf")
+let bin = document.querySelector(".bin")
+let paper = document.querySelector(".paper")
+let btnThrow = document.querySelector(".btnThrow")
+
+
+btnThrow.addEventListener("click", function(){
+paper.style.left = "1200px"
+paper.style.top = "300px"
+
+setTimeout(function(){
+paper.style.left = "1350px"
+paper.style.top = "600px"
+
+
+}, 500)
+
+})
+
